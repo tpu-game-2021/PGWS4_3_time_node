dd73b22d900cc6da3d5eda4436ad0cdffa93ca29
YutaTPU <g1927062@st.t-kougei.ac.jp>
2021-09-29 17:03:16 +0900

    ShaderGraphから回転するようにしました

--- src/Assets/Scripts/RotateBehaviourScript.cs
+++ src/Assets/Scripts/RotateBehaviourScript.cs
@@ -6,5 +6,8 @@ public class RotateBehaviourScript : MonoBehaviour
 {
     void Update()
     {
+        float rorate = 360.0f * Time.deltaTime / 10.0f;
+
+        this.transform.Rotate(0, rorate, 0);
     }
 }
