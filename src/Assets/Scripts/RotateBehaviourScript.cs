32c8c4be153fc3c7abc6fec59f3fe97ceb366751
yama0322 <g1927067@st.t-kougei.ac.jp>
2021-09-29 16:20:08 +0900

    Scene_1_Cylinder 完成

--- src/Assets/Scripts/RotateBehaviourScript.cs
+++ src/Assets/Scripts/RotateBehaviourScript.cs
@@ -6,5 +6,8 @@ public class RotateBehaviourScript : MonoBehaviour
 {
     void Update()
     {
+        float rotate = 360.0f * Time.deltaTime / 10.0f;
+
+        this.transform.Rotate(0, rotate, 0);
     }
 }
