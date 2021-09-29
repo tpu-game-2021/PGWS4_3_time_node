4cffb78b9fe28a761365d6cfbea7f4ca4ab26af9
Yoshie2019 <g1927044@st.t-kougei.ac.jp>
2021-09-29 17:43:32 +0900

    講義内で紹介された項目を追加

--- src/Assets/Scripts/RotateBehaviourScript.cs
+++ src/Assets/Scripts/RotateBehaviourScript.cs
@@ -6,5 +6,9 @@ public class RotateBehaviourScript : MonoBehaviour
 {
     void Update()
     {
+        //             360‚Å1üA1•b‚Å1‘‚¦‚é@1ü‚Ì•b”
+        float rotate = 360.0f * Time.deltaTime / 10.0f;
+
+        this.transform.Rotate(0, rotate, 0);
     }
 }
